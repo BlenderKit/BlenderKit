1dfed25a2d529ade174b787fe68907a0a6084d16
Vilem Duha <vilem.duha@gmail.com>
2021-11-23 14:17:46 +0100

    BlenderKit: check for updates automatically

--- __init__.py
+++ __init__.py
@@ -1804,7 +1804,7 @@ class BlenderKitAddonPreferences(AddonPreferences):
     auto_check_update : bpy.props.BoolProperty(
         name="Auto-check for Update",
         description="If enabled, auto-check for updates using an interval",
-        default=False)
+        default=True)
 
     updater_interval_months : bpy.props.IntProperty(
         name='Months',
@@ -1815,7 +1815,7 @@ class BlenderKitAddonPreferences(AddonPreferences):
     updater_interval_days : bpy.props.IntProperty(
         name='Days',
         description="Number of days between checking for updates",
-        default=7,
+        default=1,
         min=0,
         max=31)
 
