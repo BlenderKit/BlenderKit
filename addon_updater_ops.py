899fd75f8392c4b1373894b8094a276e1fee2ee6
agajdosi <andreas.gajdosik@gmail.com>
2022-04-06 09:55:51 +0200

    Enable .zip download, not git source for updater

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1316,8 +1316,8 @@ def select_link_function(self, tag):
     link = tag["zipball_url"]
 
     # -- Example: select the first (or only) asset instead source code --
-    # if "assets" in tag and "browser_download_url" in tag["assets"][0]:
-    # 	link = tag["assets"][0]["browser_download_url"]
+    if "assets" in tag and "browser_download_url" in tag["assets"][0]:
+        link = tag["assets"][0]["browser_download_url"]
 
     # -- Example: select asset based on OS, where multiple builds exist --
     # # not tested/no error checking, modify to fit your own needs!
