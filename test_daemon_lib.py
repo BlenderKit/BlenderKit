ac3a612a69ac53ba2e5b75e088b4b9dd8a03336b
agajdosi <andreas.gajdosik@gmail.com>
2022-09-20 11:35:01 +0200

    Add tests for download

--- test_daemon_lib.py
+++ test_daemon_lib.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import bpy
 import requests
 
-from blenderkit import daemon_lib, global_vars, paths, utils
+from blenderkit import daemon_lib, download, global_vars, paths, utils
 
 
 ### DAEMON IS NOT RUNNING ###
@@ -83,7 +83,8 @@ class Test04GetReportsDaemonRunning(unittest.TestCase):
     self.assertEqual(reports[0]['app_id'], app_id)
     self.assertEqual(reports[0]['task_type'], 'daemon_status')
 
-class Test05SearchAssets(unittest.TestCase):
+class Test05SearchAndDownloadAsset(unittest.TestCase):
+  assets_to_download = []
   def _search_asset(self, search_word, asset_type):
     addon_version = f'{global_vars.VERSION[0]}-{global_vars.VERSION[1]}-{global_vars.VERSION[2]}-{global_vars.VERSION[3]}'
     blender_version = bpy.app.version
@@ -98,25 +99,66 @@ class Test05SearchAssets(unittest.TestCase):
     response = daemon_lib.search_asset(data)
     search_task_id = response['task_id']
 
+    to_download = None
     for i in range(10):
       reports = daemon_lib.get_reports(os.getpid())
       for task in reports:
-        if search_task_id == task['task_id'] and task['status'] == 'finished' and task['result'] != {}:
-          return
+        if search_task_id != task['task_id']:
+          continue
+        if task['status'] != 'finished':
+          continue
+        if task['result'] != {}:
+          for result in task['result']['results']:
+            if result['canDownload'] == True:
+              if to_download == None:
+                to_download = result
+                continue
+              result_size = result.get('filesSize', 9999999)
+              if result_size == None:
+                result_size = 9999999
+              to_download_size = to_download.get('filesSize', 9999999)
+              if to_download_size == None:
+                to_download_size = 9999999
+              if result_size < to_download_size:
+                to_download = result
+          return to_download
+
       time.sleep(i*0.1)
     self.fail('Error waiting for search task to be reported as finished')
 
-  def test01(self): self._search_asset('cat', 'model')
-  def test02(self): self._search_asset('car', 'model')
-  def test03(self): self._search_asset('steel', 'material')
-  def test04(self): self._search_asset('paint', 'material')
-  def test05(self): self._search_asset('room', 'scene')
-  def test06(self): self._search_asset('water', 'scene')
-  def test07(self): self._search_asset('forest', 'hdr')
-  def test08(self): self._search_asset('city', 'hdr')
-  def test09(self): self._search_asset('fur', 'brush')
-  def test10(self): self._search_asset('hair', 'brush')
+  def _download_asset(self, asset_data):
+    if asset_data == None:
+      self.fail('Asset data from search are None')
 
+    download.start_download(asset_data, resolution=512, model_location=(0.0, 0.0, 0.0), model_rotation=(0.0, 0.0, 0.0))
+  
+    for _ in range(100):
+      reports = daemon_lib.get_reports(os.getpid())
+      for task in reports:
+        if task['task_type'] != 'asset_download':
+          continue
+        if task['status'] != 'finished':
+          continue
+        return
+      time.sleep(1)
+
+  #small assets are chosen here
+  def test00Search(self): self.assets_to_download.append(self._search_asset('Toy train-02', 'model'))
+  def test01Search(self): self.assets_to_download.append(self._search_asset('Wooden toy car', 'model'))
+  def test02Search(self): self.assets_to_download.append(self._search_asset('flowers1 wallpaper', 'material'))
+  def test03Search(self): self.assets_to_download.append(self._search_asset('hexa wallpaper', 'material'))
+  def test04Search(self): self.assets_to_download.append(self._search_asset('Desk for product visualization', 'scene'))
+  def test05Search(self): self.assets_to_download.append(self._search_asset('Butterfly Mural Room', 'scene'))
+  def test06Search(self): self.assets_to_download.append(self._search_asset('Garden Nook', 'hdr'))
+  def test07Search(self): self.assets_to_download.append(self._search_asset('Dark Autumn Forest', 'hdr'))
+  def test08Search(self): self.assets_to_download.append(self._search_asset('bricks', 'brush'))
+  def test09Search(self): self.assets_to_download.append(self._search_asset('Human eye iris', 'brush'))
+
+  def test10Download(self): self._download_asset(self.assets_to_download[0])
+  def test12Download(self): self._download_asset(self.assets_to_download[2])
+  def test14Download(self): self._download_asset(self.assets_to_download[4])
+  def test16Download(self): self._download_asset(self.assets_to_download[6])
+  def test18Download(self): self._download_asset(self.assets_to_download[8])
 
 ### DAEMON IS NOT RUNNING ###
 
