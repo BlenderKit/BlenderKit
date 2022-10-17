f5eabe9b9b398ea5c9714ff3b70c74e9eb5c895b
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-10-17 12:03:43 +0200

    Finalize logging into file

--- log.py
+++ log.py
@@ -1,23 +1,19 @@
 import logging
-import sys
 import os
-
+import sys
 
 from . import global_vars
 
 
 def get_formatter():
-  return logging.Formatter(fmt='blenderkit %(levelname)s: %(message)s [%(asctime)s.%(msecs)03d, %(filename)s:%(lineno)d]',datefmt='%H:%M:%S')
-
-
-def configure_terminal_logger():
-  terminal_logger = logging.getLogger("bk_terminal_logger")
-  terminal_logger.propagate = False
-  stream_handler = logging.StreamHandler()
-  terminal_logger.addHandler(stream_handler)
+  """Get default formatter for BlenderKit loggers."""
+  return logging.Formatter(fmt='blenderkit %(levelname)s: %(message)s [%(asctime)s.%(msecs)03d, %(filename)s:%(lineno)d]', datefmt='%H:%M:%S')
 
 
 def configure_bk_logger():
+  """Configure 'blenderkit' logger to which all other logs defined as `bk_logger = logging.getLogger(__name__)` writes.
+  Sets it logging level to `global_vars.LOGGING_LEVEL_BLENDERKIT`.
+  """
   logging.basicConfig(level=global_vars.LOGGING_LEVEL_BLENDERKIT)
   bk_logger = logging.getLogger("blenderkit")
   bk_logger.propagate = False
@@ -28,6 +24,7 @@ def configure_bk_logger():
 
 
 def configure_imported_loggers():
+  """Configure loggers for imported modules so they can have different logging level `global_vars.LOGGING_LEVEL_IMPORTED` than main blenderkit logger."""
   urllib3_logger = logging.getLogger("urllib3")
   urllib3_logger.propagate = False
 
@@ -37,36 +34,44 @@ def configure_imported_loggers():
   urllib3_logger.addHandler(urllib3_handler)
 
 
-def add_file_logger(global_dir: str):
+def setup_logging_to_file(global_dir: str):
+  """Setup logging to file by redirecting all stdout and stderr into dedicated loggers which logs into file and into stream (back to console), also add file handler to `blenderkit` logger.
+  The redirection is done by setting `sys.stdout` and `sys.stderr` to custom `LogFile()` object.
+  File output is located at `global_dir/blenderkit.log`.
+  """
   log_path = os.path.join(global_dir, 'blenderkit.log')
 
-  bk_logger = logging.getLogger('blenderkit')
   file_handler = logging.FileHandler(log_path)
   file_handler.setFormatter(get_formatter())
-  bk_logger.addHandler(file_handler)
+
+  # has to be done here as before we do not have access to preferences and global_dir
+  logging.getLogger('blenderkit').addHandler(file_handler)
 
   sys.stdout = LogFile('stdout', log_path)
   sys.stderr = LogFile('stderr', log_path)
-  return
 
 
 class LogFile(object):
-  """File-like object to log text using the `logging` module."""
+  """File-like object to log text using the `logging` module. This is used to replace the default `sys.stdout` and `sys.stderr`."""
 
   def __init__(self, name, log_path):
-    handler = logging.FileHandler(log_path, mode='w')
     formatter = logging.Formatter(fmt='%(message)s [%(asctime)s.%(msecs)03d, %(filename)s:%(lineno)d]', datefmt='%H:%M:%S')
-    handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(log_path, mode='w')
+    file_handler.setFormatter(formatter)
     
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
     self.logger = logging.getLogger(name)
     self.logger.propagate = False
-    self.logger.addHandler(handler)
+    self.logger.addHandler(file_handler)
+    self.logger.addHandler(stream_handler)
 
   def write(self, msg, level = logging.INFO):
     if msg.strip() == "":
       return
     self.logger.log(level, msg.rstrip())
-    #logging.getLogger("bk_terminal_logger").log(level, msg)
 
   def flush(self):
     for handler in self.logger.handlers:
@@ -74,6 +79,6 @@ class LogFile(object):
 
 
 def configure_loggers():
+  """Configure all loggers for BlenderKit addon. See called functions for details."""
   configure_bk_logger()
   configure_imported_loggers()
-  configure_terminal_logger()
