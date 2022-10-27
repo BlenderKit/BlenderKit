8cb21bacbc4bef9e7e83de305c7d56d3e7dad876
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-10-27 11:08:43 +0200

    BlenderConference 2022 release

--- __init__.py
+++ __init__.py
@@ -18,12 +18,11 @@
 
 bl_info = {
     "name": "BlenderKit Online Asset Library",
-    "author": "Vilem Duha, Petr Dlouhy",
-    "version": (3, 3, 0, 221025), #X.Y.Z.yymmdd
+    "author": "Vilem Duha, Petr Dlouhy, A. Gajdosik",
+    "version": (3, 3, 0, 221027), #X.Y.Z.yymmdd
     "blender": (3, 0, 0),
     "location": "View3D > Properties > BlenderKit",
     "description": "Online BlenderKit library (materials, models, brushes and more). Connects to the internet.",
-    "warning": "If update failed: disable addon, restart PC, remove addon, install from ZIP.",
     "doc_url": "https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html",
     "tracker_url": "https://github.com/BlenderKit/blenderkit/issues",
     "category": "3D View",
