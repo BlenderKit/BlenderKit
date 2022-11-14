325b3bb97f832cb153d8101ae6e0202adc889315
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-14 11:55:31 +0100

    Polish uploads.py file

--- daemon/uploads.py
+++ daemon/uploads.py
@@ -14,27 +14,28 @@ from aiohttp import ClientSession, web
 import utils
 
 
-BLENDERKIT_EXPORT_DATA_FILE = "data.json"
+BLENDERKIT_EXPORT_DATA_FILE = 'data.json'
 
 
 async def do_upload(request: web.Request, task: tasks.Task):
-  session = request.app["SESSION_API_REQUESTS"]
+  session = request.app['SESSION_API_REQUESTS']
 
-  ok, metadata_response = await upload_metadata(session, task)
-  if not ok:
-    logging.error('Metadata upload failed')
+  task.change_progress(1, 'posting metadata')  
+  error, metadata_response = await upload_metadata(session, task)
+  if error != '':
+    task.error(f'Metadata upload failed: {error}')
     return
-  logging.warning('Metadata upload was successfull')
 
-  ok, files = await pack_blend_file(task, metadata_response)
-  if not ok:
-    logging.error('Asset packing failed')
+  task.change_progress(5, 'packing files')  
+  error, files = await pack_blend_file(task, metadata_response)
+  if error != '':
+    task.error(f'Asset packing failed: {error}')
     return
-  logging.warning('Assets packed sucessfully')
 
-  ok = await upload_asset_data(session, task, files, metadata_response)
-  if not ok:
-    logging.error('Asset upload failed')
+  task.change_progress(20, 'uploading files')  
+  error = await upload_asset_data(session, task, files, metadata_response)
+  if error != '':
+    task.error(f'Asset upload failed: {error}')
     return
 
   task.finished('Asset successfully uploaded.')
@@ -51,7 +52,6 @@ async def upload_metadata(session: ClientSession, task: tasks.Task):
   headers = utils.get_headers(upload_data['token'])
   json_metadata = upload_data
 
-  task.change_progress(1, 'posting metadata')  
   if export_data['assetBaseId'] == '':
     try:
       response = await session.post(url, json=json_metadata, headers=headers)
@@ -59,25 +59,21 @@ async def upload_metadata(session: ClientSession, task: tasks.Task):
       metadata_response = await response.json()
     except Exception as e:
       logging.error(str(e))
-      task.error(f'Metadata upload failed.')
-      return False, None
-    return True, metadata_response
+      return str(e), None
+    return '', metadata_response
 
   try:
     url = f'{url}/{export_data["id"]}/'
     if 'MAINFILE' in upload_set:
-        json_metadata["verificationStatus"] = "uploading"
+        json_metadata['verificationStatus'] = 'uploading'
     response = await session.patch(url, json=json_metadata, headers=headers)
     logging.info(f'Got response ({response.status}) for {url}')
     metadata_response = await response.json()
   except Exception as e:
     logging.error(str(e))
-    task.error(f'Metadata upload failed.')
-    return False, None
+    return str(e), None
 
-  return True, metadata_response
-
-
+  return '', metadata_response
 
 
 async def pack_blend_file(task: tasks.Task, metadata_response):
@@ -94,7 +90,6 @@ async def pack_blend_file(task: tasks.Task, metadata_response):
   if export_data['assetBaseId'] == '':
     export_data['assetBaseId'] = metadata_response['assetBaseId']
     export_data['id'] = metadata_response['id']
-
   upload_data['assetBaseId'] = export_data['assetBaseId']
   upload_data['id'] = export_data['id']
 
@@ -113,15 +108,15 @@ async def pack_blend_file(task: tasks.Task, metadata_response):
       with open(datafile, 'w', encoding='utf-8') as s:
         json.dump(data, s, ensure_ascii=False, indent=4)
 
-      task.change_progress(5, 'preparing scene - running blender instance')
+      task.change_progress(10, 'preparing scene - running blender instance')
 
       process = await asyncio.create_subprocess_exec(
         export_data['binary_path'],
-        "--background",
-        "-noaudio",
+        '--background',
+        '-noaudio',
         cleanfile_path,
-        "--python", script_path,
-        "--", datafile,
+        '--python', script_path,
+        '--', datafile,
         stdout=None,
         stderr=None,
         stdin=None,
@@ -129,30 +124,28 @@ async def pack_blend_file(task: tasks.Task, metadata_response):
 
       await process.communicate()
       if process.returncode != 0:
-        task.error("File packing script failed with non zero return code")
-        return False, None
+        return f'file packing script failed with non zero return code ({process.returncode})', None
 
   files = []
   if 'THUMBNAIL' in upload_set:
     files.append({
-      "type": "thumbnail",
-      "index": 0,
-      "file_path": export_data["thumbnail_path"]
+      'type': 'thumbnail',
+      'index': 0,
+      'file_path': export_data['thumbnail_path']
     })
   if 'MAINFILE' in upload_set:
     files.append({
-      "type": "blend",
-      "index": 0,
-      "file_path": fpath
+      'type': 'blend',
+      'index': 0,
+      'file_path': fpath
     })
     if not os.path.exists(fpath):
-      task.error("File packing failed, please try manual packing first")
-      return False, None
+      return 'packed file does not exist, please try manual packing first', None
     
-  return True, files
+  return '', files
 
 
-async def upload_asset_data(session: ClientSession, task: tasks.Task, files: list, metadata_response: dict):
+async def upload_asset_data(session: ClientSession, task: tasks.Task, files: list, metadata_response: dict) -> str:
   """Upload .blend file and/or thumbnail to the server."""
   api_url = f'{globals.SERVER}/api/v1'
   upload_data = task.data['upload_data']
@@ -180,41 +173,40 @@ async def upload_asset_data(session: ClientSession, task: tasks.Task, files: lis
 
       if 250 > response.status > 199: #WHY?
         upload_done_url = f'{api_url}/uploads_s3/{upload_info_json["id"]}/upload-file/'
-        response = await session.post(upload_done_url, headers=headers)
+        response = await session.post(upload_done_url, headers=headers) #TODO: we should check this return value also?
+        task.change_progress(task.progress + 15)
       else:
         logging.warning(f'file upload failed, status={response.status}')
         text = await response.text()
         logging.warning(f'response={text}')
         uploaded = False
-  
-  if files == []: #metadata only case
-    uploaded = True
-  if uploaded:
-    set_uploaded_status = False
+    
 
-    #Check the status if only thumbnail or metadata gets reuploaded.
-    #the logic is that on hold assets might be switched to uploaded state for validators,
-    #if the asset was put on hold because of thumbnail only.
-    if 'MAINFILE' not in upload_set:
-      if metadata_response.get('verificationStatus') in ('on_hold','deleted','rejected'):
-        set_uploaded_status = True
+  if not uploaded:
+    return 'some files not uploaded'
 
-    if 'MAINFILE' in upload_set:
+  set_uploaded_status = False
+
+  #Check the status if only thumbnail or metadata gets reuploaded.
+  #the logic is that on hold assets might be switched to uploaded state for validators,
+  #if the asset was put on hold because of thumbnail only.
+  if 'MAINFILE' not in upload_set:
+    if metadata_response.get('verificationStatus') in ('on_hold','deleted','rejected'):
       set_uploaded_status = True
 
-    # mark on server as uploaded
-    if set_uploaded_status:
-      confirm_data = {
-        "verificationStatus": "uploaded"
-      }
-      url = f'{globals.SERVER}/api/v1/assets/'
-      headers = utils.get_headers(upload_data['token'])
+  if 'MAINFILE' in upload_set:
+    set_uploaded_status = True
 
-      url += upload_data["id"] + '/'
-      response = await session.patch(url, json=confirm_data, headers=headers)
-      if response.status != 200:
-        logging.warning('failed to confirm the upload and push is to state "uploaded"')
+  # mark on server as uploaded
+  if set_uploaded_status:
+    confirm_data = {
+      'verificationStatus': 'uploaded'
+    }
+    url = f'{globals.SERVER}/api/v1/assets/'
+    headers = utils.get_headers(upload_data['token'])
 
-    return True
-
-  return False
+    url += upload_data['id'] + '/'
+    response = await session.patch(url, json=confirm_data, headers=headers)
+    if response.status != 200:
+      return 'failed to confirm the upload'
+  return ''
