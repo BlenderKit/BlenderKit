9412149a41b60fe6932c27123659f441ec778715
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-18 15:11:00 +0100

    Increase version to 3.3.1

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "BlenderKit Online Asset Library",
     "author": "Vilem Duha, Petr Dlouhy, A. Gajdosik",
-    "version": (3, 3, 0, 221027), #X.Y.Z.yymmdd
+    "version": (3, 3, 1, 221118), #X.Y.Z.yymmdd
     "blender": (3, 0, 0),
     "location": "View3D > Properties > BlenderKit",
     "description": "Online BlenderKit library (materials, models, brushes and more). Connects to the internet.",
