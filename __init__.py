90907498822f775de10bebf06306c3e65c743d48
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-24 14:09:57 +0100

    Update formatting, set version

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "BlenderKit Online Asset Library",
     "author": "Vilem Duha, Petr Dlouhy, A. Gajdosik",
-    "version": (3, 3, 1, 221122), #X.Y.Z.yymmdd
+    "version": (3, 4, 0, 221124), #X.Y.Z.yymmdd
     "blender": (3, 0, 0),
     "location": "View3D > Properties > BlenderKit",
     "description": "Online BlenderKit library (materials, models, brushes and more). Connects to the internet.",
@@ -1849,8 +1849,8 @@ If you use HTTPS proxy, set in format https://ip:port, or https://username:passw
         default=True)
 
     enable_prereleases: bpy.props.BoolProperty(
-        name="Enable pre-releases",
-        description="If enabled, updater will include also pre-release versions",
+        name="Enable prereleases",
+        description="If enabled, updater will include also experimental prerelease versions",
         default=False)
 
     updater_interval_months: bpy.props.IntProperty(
--- global_vars.py
+++ global_vars.py
@@ -2,6 +2,7 @@ import collections
 import logging
 import os
 
+
 DAEMON_ACCESSIBLE = False
 DAEMON_ONLINE = False
 DATA = {
@@ -15,38 +16,25 @@ PREFS = {}
 SERVER = os.environ.get('BLENDERKIT_SERVER', 'https://www.blenderkit.com')
 
 TIPS = [
-  ('You can disable tips in the add-on preferences.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#preferences'),
+  ('You can disable tips in the add-on preferences.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#preferences'),
   ('Ratings help us distribute funds to creators.', f'{SERVER}/docs/rating/'),
   ('Creators also gain credits for free assets from subscribers.', f'{SERVER}/docs/fair-share/'),
-  ('Click or drag model or material in scene to link/append.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#basic-usage'),
-  ('Right click in the asset bar for a detailed asset card.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#'),
-  ('Use Append in import settings if you want to edit downloaded objects.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#import-method'),
-  ('Go to import settings to set default texture resolution.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#import-method'),
-  ('Please rate responsively and plentifully. This helps us distribute rewards to the authors.',
-   f'{SERVER}/docs/rating/'),
+  ('Click or drag model or material in scene to link/append.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#basic-usage'),
+  ('Right click in the asset bar for a detailed asset card.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#'),
+  ('Use Append in import settings if you want to edit downloaded objects.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#import-method'),
+  ('Go to import settings to set default texture resolution.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#import-method'),
+  ('Please rate responsively and plentifully. This helps us distribute rewards to the authors.', f'{SERVER}/docs/rating/'),
   ('All materials are free.', f'{SERVER}/asset-gallery?query=category_subtree:material%20order:-created'),
   ('Storage for public assets is unlimited.', f'{SERVER}/become-creator/'),
   ('Locked models are available if you subscribe to Full plan.', f'{SERVER}/plans/pricing/'),
   ('Login to upload your own models, materials or brushes.', f'{SERVER}/'),
-  ('Use \'A\' key over the asset bar to search assets by the same author.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#basic-usage'),
-  ('Use semicolon - ; to hide or show the AssetBar.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
+  ('Use \'A\' key over the asset bar to search assets by the same author.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#basic-usage'),
+  ('Use semicolon - ; to hide or show the AssetBar.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
   ('Support the authors by subscribing to Full plan.', f'{SERVER}/plans/pricing/'),
-  ('Use the W key over the asset bar to open the Author\'s webpage.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
-  ('Use the R key over the asset bar for fast rating of assets.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
-  ('Use the X key over the asset bar to delete the asset from your hard drive.',
-   'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
-  (
-  'Want to help us test the latest experimental versions of the add-on? Enable pre-releases in the add-on preferences.',
-  ''),
+  ('Use the W key over the asset bar to open the Author\'s webpage.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
+  ('Use the R key over the asset bar for fast rating of assets.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
+  ('Use the X key over the asset bar to delete the asset from your hard drive.', 'https://docs.blender.org/manual/en/3.1/addons/3d_view/blenderkit.html#assetbar'),
+  ('Get latest experimental versions of add-on by enabling prerelases in preferences.', ''),
 ]
 VERSION = None  # filled in register()
 
