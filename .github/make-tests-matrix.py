29dc54d6b7bac6bb9c9012aa48bc27244d9ad455
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2023-02-15 13:35:31 +0100

    #482 Cache Blender binaries in Github actions

--- .github/make-tests-matrix.py
+++ .github/make-tests-matrix.py
@@ -3,16 +3,20 @@ from urllib import request
 
 
 jobs = [
-  {'version': '3.3.1-LTS', 'version_x_y': '3.3', 'download_url': 'https://download.blender.org/release/Blender3.3/blender-3.3.1-linux-x64.tar.xz'},
-  {'version': '3.0.0', 'version_x_y': '3.0', 'download_url': 'https://download.blender.org/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz'},
-  {'version': '3.0.1', 'version_x_y': '3.0', 'download_url': 'https://download.blender.org/release/Blender3.0/blender-3.0.1-linux-x64.tar.xz'},
-  {'version': '3.1.0', 'version_x_y': '3.1', 'download_url': 'https://download.blender.org/release/Blender3.1/blender-3.1.0-linux-x64.tar.xz'},
-  {'version': '3.1.2', 'version_x_y': '3.1', 'download_url': 'https://download.blender.org/release/Blender3.1/blender-3.1.2-linux-x64.tar.xz'},
-  {'version': '3.2.0', 'version_x_y': '3.2', 'download_url': 'https://download.blender.org/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz'},
-  {'version': '3.2.2', 'version_x_y': '3.2', 'download_url': 'https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz'},
-  {'version': '3.3.0', 'version_x_y': '3.3', 'download_url': 'https://download.blender.org/release/Blender3.3/blender-3.3.0-linux-x64.tar.xz'},
-  {'version': '3.3.2', 'version_x_y': '3.3', 'download_url': 'https://download.blender.org/release/Blender3.3/blender-3.3.2-linux-x64.tar.xz'},
-  {'version': '3.4.0', 'version_x_y': '3.4', 'download_url': 'https://download.blender.org/release/Blender3.4/blender-3.4.0-linux-x64.tar.xz'},
+  {'version': '3.0.0', 'version_x_y': '3.0', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz'},
+  {'version': '3.0.1', 'version_x_y': '3.0', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.0/blender-3.0.1-linux-x64.tar.xz'},
+  
+  {'version': '3.1.0', 'version_x_y': '3.1', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.1/blender-3.1.0-linux-x64.tar.xz'},
+  {'version': '3.1.2', 'version_x_y': '3.1', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.1/blender-3.1.2-linux-x64.tar.xz'},
+  
+  {'version': '3.2.0', 'version_x_y': '3.2', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.2/blender-3.2.0-linux-x64.tar.xz'},
+  {'version': '3.2.2', 'version_x_y': '3.2', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz'},
+  
+  {'version': '3.3.0', 'version_x_y': '3.3', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.3/blender-3.3.0-linux-x64.tar.xz'},
+  {'version': '3.3.3', 'version_x_y': '3.3', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.3/blender-3.3.3-linux-x64.tar.xz'},
+  
+  {'version': '3.4.0', 'version_x_y': '3.4', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.4/blender-3.4.0-linux-x64.tar.xz'},
+  {'version': '3.4.1', 'version_x_y': '3.4', 'sha': 'released', 'download_url': 'https://download.blender.org/release/Blender3.4/blender-3.4.1-linux-x64.tar.xz'},
   #{'version': '', 'version_x_y': '', 'download_url': ''},
 ]
 
@@ -20,15 +24,16 @@ jobs = [
 def get_daily_builds(jobs: list):
   resp = request.urlopen('https://builder.blender.org/download/daily/')
   page = resp.read().decode('utf-8')
-  releases = re.findall(r'(https://builder.blender.org/download/daily/blender-((3\.\d)\.\d-\w+)\+\S{1,6}\.\S{12}-linux\.x86_64-release\.tar\.xz)', page)
+  releases = re.findall(r'(https://builder.blender.org/download/daily/blender-((3\.\d)\.\d-\w+)\+\S{1,6}\.(\S{12})-linux\.x86_64-release\.tar\.xz)', page)
   for release in releases:
-    job = {
+    new_job = {
       'version': release[1],
       'version_x_y': release[2],
       'download_url': release[0],
+      'sha': release[3],
     }
-    if job not in jobs:
-      jobs.append(job)
+    if new_job['version'].removesuffix('-stable') not in [job['version'] for job in jobs]:
+      jobs.append(new_job)
 
 
 get_daily_builds(jobs)
