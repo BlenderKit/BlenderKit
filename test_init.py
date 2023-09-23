42df7c53823ed4c4fdfb0235b9cedac7a77fca99
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2023-09-23 18:45:42 +0200

    Fix tests

--- test_init.py
+++ test_init.py
@@ -16,21 +16,47 @@ class Test01Registration(unittest.TestCase):
         assert global_vars.PREFS != {}
         user_preferences = bpy.context.preferences.addons["blenderkit"].preferences
         prefs = {
+            # SYSTEM STUFF
             "debug_value": bpy.app.debug_value,
             "binary_path": bpy.app.binary_path,
+            "system_id": user_preferences.system_id,
+            "app_id": os.getpid(),
+            # STATISTICS
+            "download_counter": user_preferences.download_counter,
+            "asset_popup_counter": user_preferences.asset_popup_counter,
+            "welcome_operator_counter": user_preferences.welcome_operator_counter,
+            # MAIN PREFERENCES
             "api_key": user_preferences.api_key,
             "api_key_refresh": user_preferences.api_key_refresh,
-            "system_id": user_preferences.system_id,
+            "api_key_timeout": user_preferences.api_key_timeout,
+            "experimental_features": user_preferences.experimental_features,
+            "keep_preferences": user_preferences.keep_preferences,
+            # FILE PATHS
+            "directory_behaviour": user_preferences.directory_behaviour,
             "global_dir": user_preferences.global_dir,
             "project_subdir": user_preferences.project_subdir,
-            "directory_behaviour": user_preferences.directory_behaviour,
-            "is_saved": user_preferences.directory_behaviour,
-            "app_id": os.getpid(),
+            "unpack_files": user_preferences.unpack_files,
+            # GUI
+            "show_on_start": user_preferences.show_on_start,
+            "thumb_size": user_preferences.thumb_size,
+            "max_assetbar_rows": user_preferences.max_assetbar_rows,
+            "search_field_width": user_preferences.search_field_width,
+            "search_in_header": user_preferences.search_in_header,
+            "tips_on_start": user_preferences.tips_on_start,
+            "announcements_on_start": user_preferences.announcements_on_start,
+            # NETWORK
+            "daemon_port": user_preferences.daemon_port,
             "ip_version": user_preferences.ip_version,
             "ssl_context": user_preferences.ssl_context,
             "proxy_which": user_preferences.proxy_which,
             "proxy_address": user_preferences.proxy_address,
             "trusted_ca_certs": user_preferences.trusted_ca_certs,
-            "unpack_files": user_preferences.unpack_files,
+            # UPDATES
+            "auto_check_update": user_preferences.auto_check_update,
+            "enable_prereleases": user_preferences.enable_prereleases,
+            "updater_interval_months": user_preferences.updater_interval_months,
+            "updater_interval_days": user_preferences.updater_interval_days,
+            # IMPORT SETTINGS
+            "resolution": user_preferences.resolution,
         }
         assert global_vars.PREFS == prefs
