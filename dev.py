b61251b69440941a1634deb84d3f96a928cbca64
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2023-10-12 12:49:03 +0200

    Update dependencies

--- dev.py
+++ dev.py
@@ -10,12 +10,12 @@ import global_vars
 PACKAGES = [
     "multidict==6.0.4",
     "yarl==1.9.2",
-    "aiohttp==3.8.5",
+    "aiohttp==3.8.6",
     "aiosignal==1.3.1",
-    "async-timeout==4.0.2",
+    "async-timeout==4.0.3",
     "attrs==23.1.0",
     "certifi==2023.7.22",
-    "charset-normalizer==3.2.0",
+    "charset-normalizer==3.3.0",
     "frozenlist==1.4.0",
     "idna==3.4",
 ]
@@ -176,7 +176,7 @@ def bundle_dependencies():
         for platform in OS["platforms"]:
             for module in OS["platforms"][platform]:
                 cmd = [
-                    "pip",
+                    "pip3",
                     "install",
                     "--only-binary=:all:",
                     f"--platform={platform}",
