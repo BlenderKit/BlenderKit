19ae995dd68b90d849659beb705338bc3a0f26bd
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-02-06 16:59:51 +0100

    Basic download implemented: unpacking needs to be done

--- utils.go
+++ utils.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
+	"regexp"
+	"strings"
 )
 
 // parseRequestJSON parses the JSON from the request and returns the parsed JSON and the app_id
@@ -23,16 +27,14 @@ func parseRequestJSON(r *http.Request) (map[string]interface{}, int, error) {
 }
 
 func getHeaders(apiKey, systemID string) http.Header {
-	headers := http.Header{
-		"Accept":           []string{"application/json"},
-		"Platform-Version": []string{PlatformVersion},
-		"System-Id":        []string{systemID},
-		"Addon-Version":    []string{Version},
-	}
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+	headers.Set("Platform-Version", PlatformVersion)
+	headers.Set("system-id", systemID)
+	headers.Set("addon-version", Version)
 	if apiKey != "" {
 		headers.Set("Authorization", "Bearer "+apiKey)
 	}
-
 	return headers
 }
 
@@ -100,4 +102,59 @@ type ThumbnailDownloadData struct {
 	AssetBaseID   string `json:"assetBaseId"`
 	ThumbnailType string `json:"thumbnail_type"`
 	Index         int    `json:"index"`
+}
+
+// Check if the file exists on the hard drive.
+// Returns error if the file exists but is not a file.
+func FileExists(filePath string) (bool, fs.FileInfo, error) {
+	info, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		return false, info, nil
+	}
+	if info.IsDir() {
+		return false, info, fmt.Errorf("file is a directory")
+	}
+	return true, info, nil
+}
+
+func DeleteFile(filePath string) error {
+	err := os.Remove(filePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Files on server are saved in format: "resolution_2K_d5368c9d-092e-4319-afe1-dd765de6da01.blend" for resolution files, or "blend_d5368c9d-092e-4319-afe1-dd765de6da01.blend" for original files,
+// but locally we want to store them as "asset-name_2K_d5368c9d-092e-4319-afe1-dd765de6da01.blend".
+// This is for better human readability.
+func ServerToLocalFilename(filename, assetName string) string {
+	filename = strings.Replace(filename, "blend_", "", -1)
+	filename = strings.Replace(filename, "resolution_", "", -1)
+	lfn := Slugify(assetName) + "_" + filename
+	return lfn
+}
+
+func Slugify(slug string) string {
+	slug = strings.ToLower(slug) // Normalize string: convert to lowercase
+	characters := "<>:\"/\\|?*., ()#"
+	for _, ch := range characters {
+		slug = strings.ReplaceAll(slug, string(ch), "_")
+	}
+
+	// Remove non-alpha characters, and convert spaces to hyphens.
+	reg := regexp.MustCompile(`[^a-z0-9]+`)
+	slug = reg.ReplaceAllString(slug, "-")
+	slug = strings.Trim(slug, "-")
+
+	// Replace multiple hyphens with a single one
+	reg = regexp.MustCompile(`[-]+`)
+	slug = reg.ReplaceAllString(slug, "-")
+
+	// Ensure the slug does not exceed 50 characters
+	if len(slug) > 50 {
+		slug = slug[:50]
+	}
+
+	return slug
 }
