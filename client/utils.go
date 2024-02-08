6e5f0ef84707a8e3b1a906e579af8daf989e4e20
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-02-08 14:43:57 +0100

    Parse SearchData in more direct way

--- client/utils.go
+++ client/utils.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/fs"
 	"net/http"
@@ -11,20 +10,6 @@ import (
 	"regexp"
 	"strings"
 )
-
-// parseRequestJSON parses the JSON from the request and returns the parsed JSON and the app_id
-func parseRequestJSON(r *http.Request) (map[string]interface{}, int, error) {
-	var rJSON map[string]interface{}
-	if err := json.NewDecoder(r.Body).Decode(&rJSON); err != nil {
-		return nil, -1, fmt.Errorf("error parsing JSON: %v", err)
-	}
-
-	appIDFloat, appIDExists := rJSON["app_id"].(float64)
-	if !appIDExists {
-		return nil, -1, fmt.Errorf("invalid or missing 'app_id' in JSON")
-	}
-	return rJSON, int(appIDFloat), nil
-}
 
 func getHeaders(apiKey, systemID string) http.Header {
 	headers := http.Header{}
@@ -38,34 +23,31 @@ func getHeaders(apiKey, systemID string) http.Header {
 	return headers
 }
 
-func GetAddonVersionFromJSON(rJSON map[string]interface{}) (*AddonVersion, error) {
+func StringToAddonVersion(s string) (*AddonVersion, error) {
 	adVer := &AddonVersion{}
-	verString, addonVersionExists := rJSON["addon_version"].(string)
-	if !addonVersionExists {
-		return nil, fmt.Errorf("missing 'addon_version' in JSON")
+	if s == "" {
+		return nil, fmt.Errorf("empty 'addon_version'")
 	}
-	if verString != "" {
-		_, err := fmt.Sscanf(verString, "%d.%d.%d", &adVer.Major, &adVer.Minor, &adVer.Patch)
-		if err != nil {
-			err = fmt.Errorf("error parsing 'addon_version' in JSON: %v", err)
-			return nil, err
-		}
+
+	_, err := fmt.Sscanf(s, "%d.%d.%d", &adVer.Major, &adVer.Minor, &adVer.Patch)
+	if err != nil {
+		err = fmt.Errorf("error parsing 'addon_version' in JSON: %v", err)
+		return nil, err
 	}
+
 	return adVer, nil
 }
 
-func GetBlenderVersionFromJSON(rJSON map[string]interface{}) (*BlenderVersion, error) {
+func StringToBlenderVersion(s string) (*BlenderVersion, error) {
 	blVer := &BlenderVersion{}
-	verString, blenderVersionExists := rJSON["blender_version"].(string)
-	if !blenderVersionExists {
-		return nil, fmt.Errorf("missing 'blender_version' in JSON")
+	if s == "" {
+		return nil, fmt.Errorf("empty 'blender_version'")
 	}
-	if verString != "" {
-		_, err := fmt.Sscanf(verString, "%d.%d.%d", &blVer.Major, &blVer.Minor, &blVer.Patch)
-		if err != nil {
-			err = fmt.Errorf("error parsing 'blender_version' in JSON: %v", err)
-			return nil, err
-		}
+
+	_, err := fmt.Sscanf(s, "%d.%d.%d", &blVer.Major, &blVer.Minor, &blVer.Patch)
+	if err != nil {
+		err = fmt.Errorf("error parsing 'blender_version' in JSON: %v", err)
+		return nil, err
 	}
 	return blVer, nil
 }
