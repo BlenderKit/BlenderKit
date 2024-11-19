21112c24a609d016fc955a7997f769f933b424f8
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2024-11-19 16:48:17 +0100

    Move test of production SERVER var from gh workflow to unittests

--- test_global_vars.py
+++ test_global_vars.py
@@ -20,3 +20,14 @@ class TestVersions(unittest.TestCase):
             f"v{client_version}",
             "global_vars.CLIENT_VERSION does not match the content of client/VERSION",
         )
+
+
+class TestProductionIsSet(unittest.TestCase):
+    def test_server_set_to_production(self):
+        """Ensure the SERVER variable in global_vars is set to production."""
+        expected_server = "https://www.blenderkit.com"
+        self.assertEqual(
+            global_vars.SERVER,
+            expected_server,
+            f"SERVER is not set to production. Expected: {expected_server}, Found: {global_vars.SERVER}",
+        )
