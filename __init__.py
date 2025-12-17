7e5c7b496189c98e1b7756112f7c894529dd6ddf
Andreas Gajdosik <andreas@gajdosik.org>
2025-12-17 11:46:04 +0100

    Bump up version to BlenderKit v3.18.1.251217

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "BlenderKit Online Asset Library",
     "author": "Vilem Duha, Petr Dlouhy, A. Gajdosik",
-    "version": (3, 18, 0, 251121),  # X.Y.Z.yymmdd
+    "version": (3, 18, 0, 251217),  # X.Y.Z.yymmdd
     "blender": (3, 0, 0),
     "location": "View3D > Properties > BlenderKit",
     "description": "Boost your workflow with drag&drop assets from the community driven library.",
@@ -28,7 +28,7 @@ bl_info = {
     "tracker_url": "https://github.com/BlenderKit/blenderkit/issues",
     "category": "3D View",
 }
-VERSION = (3, 18, 0, 251121)
+VERSION = (3, 18, 0, 251217)
 
 import logging
 import random
